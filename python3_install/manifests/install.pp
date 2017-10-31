6f29e1b26ddb06d726404599d6835d91c7ff8248
mudricd <mudricd@gmail.com>
2017-11-01 11:57:45 +1300

    Python34 install puppet commit

---
+++ python3_install/manifests/install.pp
@@ -0,0 +1,48 @@
+class python3_install::install {
+
+if $python3_install::present == true {
+
+  case $operatingsystemmajrelease {
+
+      '6': {
+
+  $packages = ['python34']
+  package { $packages:
+      ensure => present,
+      }
+    }
+
+     '7': {
+
+  $packages = ['python34']
+  package { $packages:
+      ensure => present,
+      }
+    }
+  }
+ }
+
+
+if $python3_install::present == false {
+
+  case $operatingsystemmajrelease {
+
+      '6': {
+
+  $packages = ['python34']
+  package { $packages:
+      ensure => purged,
+      }
+    }
+
+     '7': {
+
+  $packages = ['python34']
+  package { $packages:
+      ensure => purged,
+      }
+    }
+  }
+ }
+
+}
