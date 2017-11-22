b931b3a34be44a87a3073cba4a9968cd7865d017
mudricd <mudricd@gmail.com>
2017-11-22 15:48:45 +1300

    Uninstalls chrony

--- chrony_uninstall/uninstall.pp
+++
@@ -1,8 +0,0 @@
-class chrony_uninstall::uninstall inherits chrony_uninstall {
-
-        package {'chrony':
-          ensure => 'absent',
-        }
-
-}
-# inherits chrony_uninstall can be removed if you have puppet enterprise. I nedded it while I was working with Foreman.
