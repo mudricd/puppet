62068375e4302d3b1c21dae03131f1493df78347
mudricd <mudricd@gmail.com>
2017-11-22 15:46:46 +1300

    Uninstalls chrony

--- chrony_uninstall/manifests/uninstall.pp
+++ chrony_uninstall/manifests/uninstall.pp
@@ -1,9 +1,20 @@
 class chrony_uninstall::uninstall {
 
-	package {'chrony':
-	  provider => 'yum',       # using yum to remove package instead of rpm -e
-	  ensure   => 'purged',    # removes package and all its dependencies
-        }
+    case $operatingsystemmajrelease {
 
+      '6','7': {
+
+	package {'chrony':
+          provider => 'yum',    # using yum to remove package instead of rpm -e
+          ensure   => 'purged', # removes package and all its dependencies
+        }
+    } 
+      default: {
+        
+         notify {"Module ${module_name} is not supported on ${::operatingsystem}":}
+    }
+
+  }
+  
 }
 
