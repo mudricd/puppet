a91cb5582d142ad597a885033886f8ea1eedef60
mudricd <mudricd@gmail.com>
2017-11-27 11:39:19 +1300

    Install python34 (one param)

--- rsync_install/manifests/install.pp
+++ rsync_install/manifests/install.pp
@@ -1,7 +1,18 @@
-class rsync_install::install{
+class rsync_install::install {
 
-	package{'rsync':
-           ensure => 'present',
+  if $rsync_install::present == true {
 
+    $packages = ['rsync']
+    package { $packages:
+      ensure => present,
     }
+  }
+
+  elsif $rsync_install::present == false {
+
+    $packages = ['rsync']
+    package { $packages:
+      ensure => purged,
+    }
+  }
 }
