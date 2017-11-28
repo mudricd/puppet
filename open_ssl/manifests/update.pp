ab3908301f6a7de34903178d23bd96ad16f03483
mudricd <mudricd@gmail.com>
2017-11-28 13:57:37 +1300

    Keep open ssl packages up to date

--- open_ssl/manifests/update.pp
+++ open_ssl/manifests/update.pp
@@ -1,8 +1,23 @@
 class open_ssl::update {
 
+    case $operatingsystemmajrelease {
+
+      '6','7','5': {
+
+
       $packages = ['openssl','openssl-devel']
 
       package { $packages:
           ensure => latest,
       }
- }
+
+    }
+      default: {
+
+         echo {'Failure notification open_ssl':
+            message => 'Package openssl-devel is not available on this distribution!'
+
+      }
+    }
+  }
+}
